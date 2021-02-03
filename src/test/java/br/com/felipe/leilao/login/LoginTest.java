2f5473638773a0eb512c3c21abc632185d2592c0
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-03 17:35:35 -0300

    Testes na pagina de login

---
+++ src/test/java/br/com/felipe/leilao/login/LoginTest.java
@@ -0,0 +1,71 @@
+package br.com.felipe.leilao.login;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+	
+	private static final String URL_LOGIN = "http://localhost:8080/login";
+	private WebDriver browser;
+
+	@BeforeAll
+	public static void beforeAll() {
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		
+	}
+	
+	@BeforeEach
+	public void beforeEach() {
+		this.browser = new ChromeDriver();
+		this.browser.navigate().to(URL_LOGIN);
+		
+	}
+
+	@AfterEach
+	public void afterEach() {
+		this.browser.quit();
+		
+	}
+	
+	@Test
+	public void efetuarLoginComDadosValidos() {
+		
+		browser.findElement(By.id("username")).sendKeys("fulano");
+		browser.findElement(By.id("password")).sendKeys("pass");
+		browser.findElement(By.id("login-form")).submit();
+		
+		Assert.assertFalse(browser.getCurrentUrl().equals(URL_LOGIN));
+		Assert.assertEquals("fulano", browser.findElement(By.id("usuario-logado")).getText());
+		
+	}
+
+	@Test
+	public void naoEfetuarLoginComDadosInvalidos() {
+		
+		browser.findElement(By.id("username")).sendKeys("felipe");
+		browser.findElement(By.id("password")).sendKeys("123456");
+		browser.findElement(By.id("login-form")).submit();
+		
+		Assert.assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login?error"));
+		Assert.assertTrue("fulano", browser.getPageSource().contains("Usuário e senha inválidos."));
+		Assert.assertThrows(NoSuchElementException.class, () -> browser.findElement(By.id("usuario-logado")).getText()); //verifica se está vindo vazio
+		
+	}
+
+	@Test
+	public void naoAcessarPaginaRestritaSemEstarLogado() {
+		
+		this.browser.navigate().to("http://localhost:8080/leiloes/2");
+		
+		Assert.assertTrue(browser.getCurrentUrl().equals("http://localhost:8080/login"));
+		Assert.assertFalse(browser.getPageSource().contains("Dados do Leilão"));
+		
+	}
+}
