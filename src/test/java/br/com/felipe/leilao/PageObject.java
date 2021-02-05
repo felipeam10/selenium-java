d09bd28873f1caedf48bf2acbbb17c65f32eaf60
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-05 00:24:29 -0300

    Lidando com requisições assincronas

--- src/test/java/br/com/felipe/leilao/PageObject.java
+++ src/test/java/br/com/felipe/leilao/PageObject.java
@@ -1,5 +1,7 @@
 package br.com.felipe.leilao;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -15,6 +17,11 @@ public class PageObject {
 		} else {
 			this.browser = browser;
 		}
+		/*
+		this.browser.manage().timeouts()
+			.implicitlyWait(5, TimeUnit.SECONDS) //toda vez que o selenium for buscar um elemento na pagina ele vai esperar no maximo 5 segundos.
+			.pageLoadTimeout(10, TimeUnit.SECONDS); // se pagina nao carregar em 10 segundos, o selenium joga erro
+		*/
 	}
 	
 	public void fechar() {
--- src/test/java/br/com/felipe/leilao/login/LoginTest.java
+++ src/test/java/br/com/felipe/leilao/login/LoginTest.java
@@ -2,13 +2,8 @@ package br.com.felipe.leilao.login;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 	
